c6a67a42ad5bf7a027d99b9cb26c5f7ff2a9a4bb
Christian Hoffmann <hoffmann.c@me.com>
2026-02-03 21:53:05 +0100

    exercise 4.1 solved

---
+++ part4/bloglist/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.get('/api/blogs/:id', (request, response) => {
+  Blog.findById(request.params.id).then((blog) => {
+    response.json(blog)
+  })
+  .catch((error) => {
+    response.status(404).json({ error: 'Blog not found' })
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
