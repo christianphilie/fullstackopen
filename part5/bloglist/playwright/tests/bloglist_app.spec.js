6afecd2921313fdcab28e4d1844f09da1457ab34
Christian Hoffmann <hoffmann.c@me.com>
2026-02-10 17:42:12 +0100

    exercise 5.18 solved

--- part5/bloglist/playwright/tests/bloglist_app.spec.js
+++ part5/bloglist/playwright/tests/bloglist_app.spec.js
@@ -1,12 +1,39 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
-describe('Bloglist app', () => {
-  beforeEach(async ({ page }) => {
+describe('bloglist app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        username: 'mluukkai',
+        password: 'salainen',
+        name: 'Matti Luukkainen'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
-  test('Login form is shown', async ({ page }) => {
+  test('login form is shown', async ({ page }) => {
     const locator = page.getByText('log in to application')
     await expect(locator).toBeVisible()
+  })
+
+  describe('login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('logout')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('wrong')
+      await page.getByLabel('password').fill('wrong')
+
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('username or password', { exact: false })).toBeVisible()
+    })
   })
 })
