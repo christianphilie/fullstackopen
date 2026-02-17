a390ef51fcdb3df4c213981e65903993985f1650
Christian Hoffmann <hoffmann.c@me.com>
2026-02-17 16:46:27 +0100

    exercise 7.7 solved

--- part7/countries-hook/src/App.jsx
+++ part7/countries-hook/src/App.jsx
@@ -18,7 +18,19 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    setCountry(null)
+    if (name) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then(response => {
+          setCountry({ data: response.data, found: true })
+        })
+        .catch(() => {
+          setCountry({ data: null, found: false })
+        })
+    }
+  }, [name])
 
   return country
 }
@@ -38,10 +50,10 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
+      <h3>{country.data.name.common} </h3>
       <div>capital {country.data.capital} </div>
       <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.flags.alt}`}/>  
     </div>
   )
 }
