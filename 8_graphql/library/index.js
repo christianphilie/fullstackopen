f9bcb9857e5ee3055861015681dd1e4d01df6f4a
Christian Hoffmann <hoffmann.c@me.com>
2026-02-19 17:07:27 +0100

    feat(8.1): add bookCount and authorCount queries

--- 8_graphql/library/index.js
+++ 8_graphql/library/index.js
@@ -85,19 +85,17 @@ let books = [
   },
 ];
 
-/*
-  You can remove the placeholder query once your first one has been implemented 
-*/
-
-const typeDefs = `
+const typeDefs = /* GraphQL */ `
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `;
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
   },
 };
 
